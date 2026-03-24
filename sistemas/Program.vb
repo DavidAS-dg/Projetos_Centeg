d95122a7217cbead344e8debc068b54224a5926d
DavidAS-dg <allysond0713@gmail.com>
2026-03-24 20:47:40 -0300

    reforcing

---
+++ sistemas/Program.vb
@@ -0,0 +1,18 @@
+Imports System
+
+Module Program
+    Sub Main(args As String())
+        dim sistemas(3) as String
+
+        sistemas(0) = "Linux"
+        sistemas(1) = "Windows"
+        sistemas(2) = "IOS"
+        
+        Console.writeline(sistemas(0))
+        Console.writeline(sistemas(1))
+        Console.writeline(sistemas(2))
+
+
+
+    End Sub
+End Module
