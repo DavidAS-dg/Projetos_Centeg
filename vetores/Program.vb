292417098342958a4b65eea3a2d1583c38fcd9af
DavidAS-dg <allysond0713@gmail.com>
2026-03-24 20:17:07 -0300

    vetores

---
+++ vetores/Program.vb
@@ -0,0 +1,16 @@
+Imports System
+
+Module Program
+    Sub Main(args As String())
+        dim nomecompleto(2) as String
+
+        nomecompleto(0) = console.writeline("digite seu nome: ")
+        nomecompleto(1) = console.writeline("Agora seu sobrenome:")
+        
+        console.writeline(nomecompleto(0))
+        console.writeline(nomecompleto(1))
+
+
+
+    End Sub
+End Module
