e5c949b9653d73eb5cd82d84f906c126bb3dcd85
DavidAS-dg <allysond0713@gmail.com>
2026-03-26 20:59:06 -0300

    tabuada

---
+++ arrays/Program.vb
@@ -0,0 +1,14 @@
+Imports System
+
+Module Program
+    Sub Main(args As String())
+        dim tabuada as integer
+        
+
+        for tabuada = 1 to 10
+            console.writeline("5 * "& tabuada &" = "& 5 * tabuada)
+        next
+        
+
+    End Sub
+End Module
